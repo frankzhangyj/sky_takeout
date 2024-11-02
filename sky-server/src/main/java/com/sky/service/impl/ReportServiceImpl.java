1e925f3d2f3d526f826dbc4261c660df9ef9463b
frank <2732876164@qq.com>
2024-11-02 11:22:04 +0800

    admin order count statistics visualization model fixed

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -6,6 +6,7 @@ import com.sky.entity.User;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
@@ -90,12 +91,19 @@ public class ReportServiceImpl implements ReportService {
         }
 
         return UserReportVO.builder()
-                .dateList(StringUtils.join(dateList,","))
+                .dateList(StringUtils.join(dateList, ","))
                 .newUserList(StringUtils.join(newUserList, ","))
                 .totalUserList(StringUtils.join(totalUserList, ","))
                 .build();
     }
 
+    /**
+     * 根据起止时间得到用户数量
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
     private Integer getUserCount(LocalDateTime begin, LocalDateTime end) {
         LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.gt(begin != null, User::getCreateTime, begin)
@@ -104,5 +112,65 @@ public class ReportServiceImpl implements ReportService {
         Integer users = userMapper.selectCount(queryWrapper).intValue();
 
         return users;
+    }
+
+    @Override
+    public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        // 每天订单数集合
+        List<Integer> orderCountList = new ArrayList<>();
+        // 每天有效订单数集合
+        List<Integer> validOrderCountList = new ArrayList<>();
+
+        for (LocalDate localDate : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(localDate, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(localDate, LocalTime.MAX);
+
+            Integer orderCount = getOrderCount(beginTime, endTime, null);
+            Integer validOrderCount = getOrderCount(beginTime, endTime, Orders.COMPLETED);
+
+            orderCountList.add(orderCount);
+            validOrderCountList.add(validOrderCount);
+        }
+
+        Integer totalOrderCount = orderCountList.stream().reduce(Integer::sum).get();
+        Integer validTotalOrderCount = validOrderCountList.stream().reduce(Integer::sum).get();
+        // 计算完成率
+        Double orderCompletionRate = 0.0;
+        if (totalOrderCount != 0) {
+            orderCompletionRate = validTotalOrderCount.doubleValue() / totalOrderCount;
+        }
+
+        return OrderReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .orderCountList(StringUtils.join(orderCountList, ","))
+                .validOrderCountList(StringUtils.join(validOrderCountList, ","))
+                .totalOrderCount(totalOrderCount)
+                .validOrderCount(validTotalOrderCount)
+                .orderCompletionRate(orderCompletionRate)
+                .build();
+    }
+
+    /**
+     * 根据起止时间得到订单数量
+     * @param begin
+     * @param end
+     * @param status
+     * @return
+     */
+    private Integer getOrderCount(LocalDateTime begin, LocalDateTime end, Integer status) {
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.gt(begin != null, Orders::getOrderTime, begin)
+                .lt(end != null, Orders::getOrderTime, end)
+                .eq(status != null, Orders::getStatus, status);
+
+        Integer orderCount = orderMapper.selectCount(queryWrapper).intValue();
+
+        return orderCount;
     }
 }
